951723e68f02eccf19eeca8eb43f4977ee77faa4
flh-raouf <155684085+flh-raouf@users.noreply.github.com>
2025-03-03 01:11:00 +0100

    Prepare for deployment 3.0

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
 
     try {
       // Send a POST request to the login endpoint
-      const response = await fetch('https://gestion-piece-app.vercel.app/api/login', {
+      const response = await fetch('https://gestion-piece.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
